6988e69716aa177a89c98b04e59a41cbc84e56b7
Soumik Mukherjee <mukherjeesoumik700@gmail.com>
2022-05-30 18:34:42 +0530

    commit

--- array.c
+++ array.c
@@ -121,8 +121,18 @@ int main(){
 // // 2nd highst
 
 int main(){
-    int a=8714,b=10;
-    int j=a%b;
-    printf("%d",j);
+    int a[5],b[5],c[5],i;
+    printf("Enter the 1st array :\n");
+    for(i=0;i<5;i++){
+        scanf("%d",&a[i]);
+    }
+    printf("Enter the 2nd array :\n");
+    for(i=0;i<5;i++){
+        scanf("%d",&b[i]);
+    }
+    for(i=0;i<5;i++){
+        c[i]=a[i]+b[i];
+        printf("The sum of %d + %d = %d\n",a[i],b[i],c[i]);
+    }
     return 0;
 }
